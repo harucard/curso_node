7acce00d4323bde3800c3a1b11c700a276310701
Roquelson <roquelsonferreira@assistevida.com>
2021-07-15 20:13:36 -0300

    aula04

--- aula03/main.js
+++ aula03/main.js
@@ -1,4 +1,5 @@
 var fatorial = require('./fatorial');
 console.log('n-fatorial');
-var num = parseInt(process.argv[2]);
+var argv = require('yargs').demandOption('num').argv;
+var num = argv.num;
 console.log("O fatorial de " + num + " \u00E9 igual a " + fatorial(num));
--- aula03/main.ts
+++ aula03/main.ts
@@ -2,7 +2,9 @@ const fatorial = require('./fatorial')
 
 console.log('n-fatorial')
 
-const num = parseInt(process.argv[2])
+const argv = require('yargs').demandOption('num').argv
+
+const num = argv.num
 
 console.log(`O fatorial de ${num} é igual a ${fatorial(num)}`)
 
